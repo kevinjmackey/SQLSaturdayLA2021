b556413f862e8bd360026afa9f75c0368cfc2742
Kevin Mackey <kjmackey@hotmail.com>
2021-06-12 10:39:25 -0500

    initial load of repo

---
+++ GenerateCRUD/GenerateCRUD/DVConnection.py
@@ -0,0 +1,28 @@
+import os
+
+def GetCodeGenerationConnectionString():
+    connectionStringLocal = r"Driver={SQL Server};Server=localhost,15789;Database=WWI_Preparation;uid=sa;pwd=<Some big secret>;"
+
+    user = "sqladmin"
+    password = "Dataview#1"
+    server = "dataview.database.windows.net"
+    database = "CodeGeneration"
+
+    connectionStringAzure = r"Driver={{{0}}};".format('ODBC Driver 13 for SQL Server') + 'Server={};Database={};UID={};PWD={};'.format(server, database, user, password)
+
+    try:
+        domain = os.environ['userdomain']
+    except:
+        pass
+
+    if (domain and domain != "ERG-PREDATOR"):
+        return connectionStringAzure
+
+    return connectionStringLocal
+
+
+def main():
+    print(GetCodeGenerationConnectionString())
+
+if __name__ == "__main__":
+    main()
---
+++ Readme.md
@@ -0,0 +1,3 @@
+# Data/SQL Saturday LA 2021
+
+Repo for the presentation and code samples
