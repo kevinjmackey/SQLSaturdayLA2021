fa8a07c16c27a18efe539e7f975af5fb6ad6275f
Kevin Mackey <kjmackey@hotmail.com>
2021-06-17 11:59:33 -0500

    clean up code references

--- GenerateCRUD/GenerateCRUD/DVConnection.py
+++ GenerateCRUD/GenerateCRUD/DVConnection.py
@@ -3,9 +3,9 @@ import os
 def GetCodeGenerationConnectionString():
     connectionStringLocal = r"Driver={SQL Server};Server=localhost,15789;Database=WWI_Preparation;uid=sa;pwd=<Some big secret>;"
 
-    user = "sqladmin"
-    password = "Dataview#1"
-    server = "dataview.database.windows.net"
+    user = "<some user>"
+    password = "<Some big secret>"
+    server = "<some server>.database.windows.net"
     database = "CodeGeneration"
 
     connectionStringAzure = r"Driver={{{0}}};".format('ODBC Driver 13 for SQL Server') + 'Server={};Database={};UID={};PWD={};'.format(server, database, user, password)
@@ -15,7 +15,7 @@ def GetCodeGenerationConnectionString():
     except:
         pass
 
-    if (domain and domain != "ERG-PREDATOR"):
+    if (domain and domain != "<the laptop I am on>"):
         return connectionStringAzure
 
     return connectionStringLocal
